1e43ab0501e279bbe9f01962d42f6a99e4824c15
Damian <7105046+damianbenente@users.noreply.github.com>
2025-10-25 04:45:09 -0300

    Add multiple ERC20 token entries

--- src/v1/module/ERC20Tokens.ts
+++ src/v1/module/ERC20Tokens.ts
@@ -10,6 +10,32 @@ export const erc20Tokens: Record<string, ERC20TokenMetadata> = {
     identifier: SubProtocolSymbols.RBTC_ERC20,
     contractAddress: '0x2acc95758f8b5f583470ba265eb685a8f45fc9d5',
     decimals: 18
+  },
+    'rbtc-erc20': {
+    symbol: 'DOC',
+    name: 'Dollar On Chain',
+    marketSymbol: 'doc',
+    identifier: SubProtocolSymbols.RBTC_ERC20,
+    contractAddress: '0xE700691Da7B9851F2F35f8b8182C69C53ccad9DB',
+    decimals: 18
+  },
+  
+    'rbtc-erc20': {
+    symbol: 'BITP',
+    name: 'BitPRO ',
+    marketSymbol: 'bitp',
+    identifier: SubProtocolSymbols.RBTC_ERC20,
+    contractAddress: '0x440cd83C160de5C96DDDb20246815EA44C7Abbca8',
+    decimals: 18
+  },
+  
+    'rbtc-erc20': {
+    symbol: 'MOC',
+    name: 'Money On Chain',
+    marketSymbol: 'moc',
+    identifier: SubProtocolSymbols.RBTC_ERC20,
+    contractAddress: '0x9aC7Fe28967b30e3a4E6E03286D715B42B453d10',
+    decimals: 18
   }
 }
 
